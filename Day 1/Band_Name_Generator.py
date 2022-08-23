7e3a6394ecc42c29cae0117a4edaaec3faa6e224
Kofi Junior Eshun <eshunkofi23@gmail.com>
2022-08-23 17:19:46 -0600

    Day 1 Commit - by Kofi Junior

---
+++ Day 1/Band_Name_Generator.py
@@ -0,0 +1,7 @@
+""" This program asks you for the name of the city you grew up in,
+the name of your pet and the uses that information to generate a band name for you."""
+
+city_name = input("What is the name of the City you grew up in?\n")
+pet = input("What's your pet's name.\n")
+
+print("Your band name could be {} {}.".format(city_name, pet))
